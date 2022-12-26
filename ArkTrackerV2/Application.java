8d2dbffbf9de34243438ba34dcfec209d1d27cb7
Bevans13 <futurepf16@gmail.com>
2022-12-25 18:10:54 -0600

    Enter

--- ArkTrackerV2/Application.java
+++ ArkTrackerV2/Application.java
@@ -1,10 +1,16 @@
 package ArkTrackerV2;
 
+import javax.swing.*;
+
 public class Application {
     
 
     public static void main(String[] args) {
-        
-        
+         
+    } 
+
+    public static void initWindow(JFrame jframe) {
+
     }
+    
 }
