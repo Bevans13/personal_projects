67d4d6e9f773a48cb15cdb3c6e9de74bf7198e86
Bevans13 <futurepf16@gmail.com>
2023-01-03 00:14:06 -0600

    Enter

---
+++ ArkTrackerV3.0.57/Creatures/Pulmonoscorpius.java
@@ -0,0 +1,10 @@
+package Creatures;
+
+public class Pulmonoscorpius extends Creature{
+
+
+    public Pulmonoscorpius(String n, String sp, char gen, double health, double stam, double oxy, double f, double w,
+            double mel, double move, double torp) {
+        super(n, sp, gen, health, stam, oxy, f, w, mel, move, torp);
+    }
+}
