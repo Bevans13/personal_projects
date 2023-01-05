abdaa347f5eba0bd8e49cf75548b889c2a2229ff
Bevans13 <futurepf16@gmail.com>
2023-01-04 18:41:08 -0600

    Enter

---
+++ ArkTrackerV3.0.71/Creatures/Megatherium.java
@@ -0,0 +1,10 @@
+package Creatures;
+
+public class Megatherium extends Creature{
+
+
+    public Megatherium(String n, String sp, char gen, double health, double stam, double oxy, double f, double w,
+            double mel, double move, double torp) {
+        super(n, sp, gen, health, stam, oxy, f, w, mel, move, torp);
+    }
+}
