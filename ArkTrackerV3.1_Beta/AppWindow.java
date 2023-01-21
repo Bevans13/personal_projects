24734fc6b33621934c4ad92a366cb402fdb9ad43
Bevans13 <futurepf16@gmail.com>
2023-01-21 16:31:18 -0600

    Enter

---
+++ ArkTrackerV3.1_Beta/AppWindow.java
@@ -0,0 +1,20 @@
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+/**
+ * AppWindow
+ */
+public class AppWindow extends JFrame{
+
+    public AppWindow(String title){
+        super(title);
+    }
+
+    public AppWindow() {
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
