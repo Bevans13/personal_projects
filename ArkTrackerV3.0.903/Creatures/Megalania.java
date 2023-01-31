591ad7253a11eeeb131e061be35c30e3315d8397
Bevans13 <futurepf16@gmail.com>
2023-01-30 21:56:54 -0600

    Enter

---
+++ ArkTrackerV3.0.903/Creatures/Megalania.java
@@ -0,0 +1,8 @@
+package Creatures;
+
+public class Megalania extends Creature {
+    public Megalania(String n, String sp, char gen, double health, double stam, double oxy,
+    double f, double w, double mel, double move, double torp){
+        super(n, sp, gen, health, stam, oxy, f, w, mel, move, torp);
+    }
+}
