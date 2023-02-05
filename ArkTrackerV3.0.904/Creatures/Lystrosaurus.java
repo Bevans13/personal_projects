808ee4305d264f9ceb36a52979d44261d5a74a55
Bevans13 <futurepf16@gmail.com>
2023-02-04 19:06:13 -0600

    Enter

--- ArkTrackerV3.0.904/Creatures/Lystrosaurus.java
+++
@@ -1,8 +0,0 @@
-package Creatures;
-
-public class Lystrosaurus extends Creature {
-    public Lystrosaurus(String n, String sp, char gen, double health, double stam, double oxy,
-    double f, double w, double mel, double move, double torp){
-        super(n, sp, gen, health, stam, oxy, f, w, mel, move, torp);
-    }
-}
--- ArkTrackerV3.1.04/Creatures/World.java
+++
@@ -1,5 +0,0 @@
-package Creatures;
-
-public enum World {
-
-}
