70941c04fbc582a58150fc5531c340f2a21efd0e
Bevans13 <futurepf16@gmail.com>
2023-02-05 11:23:18 -0600

    enter

---
+++ ArkTrackerV3.1.06/Creatures/Plesiosaur.java
@@ -0,0 +1,10 @@
+package Creatures;
+
+public class Plesiosaur extends Creature {
+
+    public Plesiosaur(String n, String sp, char gen, double health, double stam, double oxy, double f, double w,
+            double mel, double move, double torp, Variation v) {
+        super(n, sp, gen, health, stam, oxy, f, w, mel, move, torp, v);
+    }
+    
+}
