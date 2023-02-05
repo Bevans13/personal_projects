6c2bd4f9cea2d6117bafd036e8ae2978923cd4f7
Bevans13 <futurepf16@gmail.com>
2023-02-05 08:48:38 -0600

    Enter

---
+++ ArkTrackerV3.1.04/Creatures/Gallimimus.java
@@ -0,0 +1,8 @@
+package Creatures;
+
+public class Gallimimus extends Creature {
+    public Gallimimus(String n, String sp, char gen, double health, double stam, double oxy,
+    double f, double w, double mel, double move, double torp, Variation v){
+        super(n, sp, gen, health, stam, oxy, f, w, mel, move, torp, v);
+    }
+}
---
+++ ArkTrackerV3.1.04/Creatures/Pegomastax.java
@@ -0,0 +1,9 @@
+package Creatures;
+
+public class Pegomastax extends Creature{
+
+    public Pegomastax(String n, String sp, char gen, double health, double stam, double oxy, double f, double w,
+        double mel, double move, double torp, Variation v) {
+        super(n, sp, gen, health, stam, oxy, f, w, mel, move, torp, v);
+    }
+}
