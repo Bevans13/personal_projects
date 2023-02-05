9a4764f02133fe088847c7371adcadec816ae012
Bevans13 <futurepf16@gmail.com>
2023-02-05 08:58:36 -0600

    Enter

---
+++ ArkTrackerV3.1.05/Creatures/Brontosaurus.java
@@ -0,0 +1,10 @@
+package Creatures;
+
+public class Brontosaurus extends Creature{
+
+
+    public Brontosaurus(String n, String sp, char gen, double health, double stam, double oxy, double f, double w,
+            double mel, double move, double torp, Variation v) {
+        super(n, sp, gen, health, stam, oxy, f, w, mel, move, torp, v);
+    }
+}
---
+++ ArkTrackerV3.1.05/Creatures/Coelacanth.java
@@ -0,0 +1,10 @@
+package Creatures;
+
+public class Coelacanth extends Creature{
+    
+    public Coelacanth(String n, String sp, char gen, double health, double stam, double oxy,
+    double f, double w, double mel, double move, double torp, Variation v){
+        super(n, sp, gen, health, stam, oxy, f, w, mel, move, torp, v);
+    }
+
+}
